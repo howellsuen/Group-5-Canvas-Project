cb7995d5fc55d48870f4a2381d4b44d37945aef4
Michelle Lau <michellelkys@gmail.com>
2018-05-29 12:22:48 +0800

    Added initial draft

---
+++ canvas-common.js
@@ -0,0 +1,58 @@
+let canvasReal = document.getElementById('canvas-real');
+let contextReal = canvasReal.getContext('2d');
+canvasReal.width = window.innerWidth;
+canvasReal.height = window.innerHeight;
+
+let canvasDraft = document.getElementById('canvas-draft');
+let contextDraft = canvasDraft.getContext('2d');
+canvasDraft.width = window.innerWidth;
+canvasDraft.height = window.innerHeight;
+
+let currentFunction;
+let dragging = false;
+
+$('#canvas-draft').mousedown(function(e){
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseDown([mouseX,mouseY],e);
+    dragging = true;
+});
+
+$('#canvas-draft').mousemove(function(e){
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    if(dragging){
+        currentFunction.onDragging([mouseX,mouseY],e);
+    }
+    currentFunction.onMouseMove([mouseX,mouseY],e);
+});
+
+$('#canvas-draft').mouseup(function(e){
+    dragging = false;
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseUp([mouseX,mouseY],e);
+});
+
+$('#canvas-draft').mouseleave(function(e){
+    dragging = false;
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseLeave([mouseX,mouseY],e);
+});
+
+$('#canvas-draft').mouseenter(function(e){
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseEnter([mouseX,mouseY],e);
+});
+
+class PaintFunction{
+    constructor(){}
+    onMouseDown(){}
+    onDragging(){}
+    onMouseMove(){}
+    onMouseUp(){}
+    onMouseLeave(){}
+    onMouseEnter(){}
+}    
---
+++ draw-circle.js
@@ -0,0 +1,44 @@
+class DrawingCircle extends PaintFunction{
+    constructor(contextReal,contextDraft){
+        super();
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;            
+    }
+    
+    onMouseDown(coord,event){
+        
+        this.contextReal.beginPath();
+        this.origX = coord[0];
+        this.origY = coord[1];
+    }
+    onDragging(coord,event){
+        this.contextDraft.fillStyle = "#44C9DD";
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.beginPath();
+        this.contextDraft.arc(this.origX, this.origY, Math.abs(coord[0]- this.origX), 0, 2 * Math.PI, false);
+        this.contextDraft.closePath();
+        contextDraft.fill();
+        //this.contextReal.stroke(); 
+    }
+
+    onMouseMove(){}
+    onMouseUp(coord){
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextReal.arc(this.origX, this.origY, Math.abs(coord[0]- this.origX), 0, 2 * Math.PI, false);
+        this.contextReal.fillStyle = "#44C9DD";
+        this.contextReal.closePath();
+        this.contextReal.fill();
+        
+    }
+    onMouseLeave(){}
+    onMouseEnter(){}
+}
+
+
+
+ // draw circles: ctx.arc(x, y, radius, startAngle, endAngle [, anticlockwise]);
+        // Pythagorean Theorem: a**2 + b**2 = c**2
+        // let w = coord[0]- this.origX;
+        // let h = coord[1] - this.origY;
+        // let radius = Math.sqrt(w**2 + h**2);
+        // this.contextDraft.arc(this.origX, this.origY, radius/2, 0, 2 * Math.PI, false);
